d255b1f7d2c60922baf4ded65ce0da94553f2f38
Ulrich Ludolfinger <laulilud@gmail.com>
2019-08-19 15:47:33 +0200

    Error correction for insertion sort

--- src/components/sorter/InsertionSort.js
+++ src/components/sorter/InsertionSort.js
@@ -13,19 +13,14 @@ class InsertionSort extends React.Component {
     this.sort = this.sort.bind(this);
   }
 
-  /*
-Läuft noch nicht ganz korrekt bei mehreren Zahlen wie z.B. 29 überschreibt er eine Zahl mit der nächstgrößeren
-Fehler währe mit debuggen leichter nachzuvollziehen
-Erst weiter Implementieren: Button: Go, Step
-*/
   sort() {
     let numbers = this.state.data;
     let curRun = this.state.sortingState;
     let ended = false;
-    if (curRun[0] === 1 && curRun[1] === 1) {
-      curRun[2] = numbers[0];
-    }
     if (curRun[0] < numbers.length) {
+      if (curRun[0] === curRun[1]) {
+        curRun[2] = numbers[curRun[0]];
+      }
       if (curRun[1] > 0 && numbers[curRun[1] - 1] > curRun[2]) {
         numbers[curRun[1]] = numbers[curRun[1] - 1];
         curRun[1]--;
@@ -33,7 +28,6 @@ Erst weiter Implementieren: Button: Go, Step
         numbers[curRun[1]] = curRun[2];
         curRun[0]++;
         curRun[1] = curRun[0];
-        curRun[2] = numbers[curRun[0]];
       }
     } else {
       ended = true;
