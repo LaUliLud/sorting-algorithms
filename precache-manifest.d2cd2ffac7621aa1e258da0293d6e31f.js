a74bc142acdd781c1dceb00252532f9c9c5ff808
Ulrich Ludolfinger <laulilud@gmail.com>
2019-09-04 06:13:37 +0200

    Updates

---
+++ precache-manifest.d2cd2ffac7621aa1e258da0293d6e31f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d56850a891bd353ddec7d6e8fb5dcc51",
+    "url": "/sorting-algorithms/index.html"
+  },
+  {
+    "revision": "538889592f7996a9f790",
+    "url": "/sorting-algorithms/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4eccff01191965d7fa98",
+    "url": "/sorting-algorithms/static/css/main.45861114.chunk.css"
+  },
+  {
+    "revision": "538889592f7996a9f790",
+    "url": "/sorting-algorithms/static/js/2.12c7caf1.chunk.js"
+  },
+  {
+    "revision": "4eccff01191965d7fa98",
+    "url": "/sorting-algorithms/static/js/main.c7cdf349.chunk.js"
+  },
+  {
+    "revision": "0ae976579a1a2ca17027",
+    "url": "/sorting-algorithms/static/js/runtime~main.f497c6c5.js"
+  },
+  {
+    "revision": "a2621a557a72d70a2e4cecddc6847c67",
+    "url": "/sorting-algorithms/static/media/code.a2621a55.jpg"
+  }
+]);
